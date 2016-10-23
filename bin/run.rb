e83165b2936c24cbd6afd5efcdc8653922b1c82d
Dan Cahoon <dpcahoon@gmail.com>
2016-10-23 10:38:56 -0400

    Use ENV for fooda url

--- bin/run.rb
+++ bin/run.rb
@@ -5,7 +5,7 @@ require_relative '../lib/fooda-bot'
 slack = Slacker.new(Slack::Web::Client.new)
 storage = Storage.new
 
-event = Event.new(RestClient.get('fooda.com/idb'))
+event = Event.new(RestClient.get(ENV['FOODA_URL']))
 
 if event && event.today?
   reactions = slack.reactions(storage.lookup(event.name))
