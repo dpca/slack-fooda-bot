a15719b2f3897fa99f51476c09a1ddcc66858155
Dan Cahoon <dpcahoon@gmail.com>
2017-11-07 11:05:02 -0500

    Fix asking for ratings

--- bin/ask_for_ratings.rb
+++ bin/ask_for_ratings.rb
@@ -8,8 +8,8 @@ storage = Storage.new(Redis.new)
 event = storage.get_latest
 
 if event && TimeHelpers.today?(event.time)
-  event.restaurants.each do |restaurant|
-    message = slack.send("How did you like #{restaurant.name}? React with an emoji on this message!")
-    storage.push(restaurant.name, message.ts) if message
+  event.names.each do |restaurant|
+    message = slack.send("How did you like #{restaurant}? React with an emoji on this message!")
+    storage.push(restaurant, message.ts) if message
   end
 end
