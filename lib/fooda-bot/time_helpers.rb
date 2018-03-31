6be465b5bbb4ea684e6dd6e2259accd8b3bc4983
Dan Cahoon <dpcahoon@gmail.com>
2018-03-31 10:12:41 -0400

    Fix tomorrow? to account for last day of month

--- lib/fooda-bot/time_helpers.rb
+++ lib/fooda-bot/time_helpers.rb
@@ -8,8 +8,8 @@ module TimeHelpers
 
   def self.tomorrow?(time)
     now = Time.new
-    today_start = Time.new(now.year, now.month, now.day + 1)
-    today_end = today_start + 86399
-    (today_start..today_end).cover?(time)
+    tomorrow_start = Time.new(now.year, now.month, now.day) + 86400
+    tomorrow_end = tomorrow_start + 86399
+    (tomorrow_start..tomorrow_end).cover?(time)
   end
 end
